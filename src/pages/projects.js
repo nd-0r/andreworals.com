c9d8aa1e30488524d187b5bee497e3b1365d62f1
Andrew Orals <nd-0r@andreworals.com>
2021-07-13 01:13:33 -0500

    Add project page

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Projects = () => {
+  return (
+    <Layout pageTitle="Projects">
+      <p>some of my projects :)</p>
+    </Layout>
+  )
+}
+
+export default Projects;

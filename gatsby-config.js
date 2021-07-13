84af2db02059fe04943072d1aba9c15491b6c195
Andrew Orals <nd-0r@andreworals.com>
2021-07-13 01:12:21 -0500

    Update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "andreworals.com",
+    title: "Andreworals.com",
   },
   plugins: [
     "gatsby-plugin-image",
